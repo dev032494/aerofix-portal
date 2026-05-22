da350f039a063d1ab04fbf8ce96c5d48f2ac5dc2
dev032494 <josephorencio032494@gmail.com>
2026-05-22 22:56:47 +0800

    fix allowedHosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5001,
-    allowHosts: ['localhost', '127.0.0.1','welcoming-smile-production-a501.up.railway.app'],
+    server: {
+      allowedHosts: [".up.railway.app"],
+    },
   },
-})
+});
