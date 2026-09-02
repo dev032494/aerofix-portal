f54d22ef5827a2b630757a6ad012090c35c33b0a
dev032494 <josephorencio032494@gmail.com>
2026-09-02 20:48:43 +0800

    open login

--- client/src/components/LoginView.jsx
+++ client/src/components/LoginView.jsx
@@ -75,7 +75,7 @@ export default function LoginView({ onLoginSuccess }) {
       const isOpen = isWeekday && isDuringHours;
 
       // console.log(`Day: ${day}, Hour: ${hours} - School hours status: ${isOpen}`);
-      setIsSchoolHours(isOpen);
+      setIsSchoolHours(true);
     };
 
     checkGateStatus();
